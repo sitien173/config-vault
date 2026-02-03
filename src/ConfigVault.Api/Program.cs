8d78b693e8dc4f0a9f945d211202a85127b07c00
Tien Ngo <sitienbmt@gmail.com>
2026-02-03 21:02:52 +0700

    feat(api): register SseConnectionManager in DI

--- src/ConfigVault.Api/Program.cs
+++ src/ConfigVault.Api/Program.cs
@@ -1,12 +1,16 @@
 using ConfigVault.Api.Middleware;
+using ConfigVault.Api.Services;
 using ConfigVault.Core.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddConfigVault(builder.Configuration);
+builder.Services.AddSingleton<SseConnectionManager>();
 
 var app = builder.Build();
+
+_ = app.Services.GetRequiredService<SseConnectionManager>();
 
 app.UseApiKeyAuth();
 app.MapControllers();
